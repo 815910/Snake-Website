855b5ed85079aa4e47a755a7883c89c1e146e83e
815910 <steele.j.alexander@gmail.com>
2019-06-18 16:28:19 -0400

    Added a grid
    Columns x position moved +0.5 to make lines sharp

--- draw.js
+++ draw.js
@@ -1,42 +1,52 @@
 //Instaniate all variables
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-//Width and height of canvas
-const width = 500;
-const height = 500;
-//Sets width and height of canvas to width and height variables
-canvas.style.setProperty('--canvas-width', width + 'px');
-canvas.style.setProperty('--canvas-height', height + 'px');
+//Pixel width and height of canvas(for CSS)
+const pxWidth = 900;
+const pxHeight = 600;
+//Sets width and height of canvas to pixel width and height variables
+canvas.style.setProperty('--canvas-width', pxWidth + 'px');
+canvas.style.setProperty('--canvas-height', pxHeight + 'px');
+//Real width and height
+const width = 300;
+const height = 150;
 //Sets the scale and number of rows and columns
 const scale = 10;
-const rows = width/scale;
-const columns = height/scale;
+const rows = height/scale;
+const columns = width/scale;
 //Instaniate all objects
 var snake = new Snake();
 
 
 function drawGrid() {
   var i;
-  // for(i = 0; i < (rows/scale); i++){
-  //   ctx.beginPath();
-  //   ctx.lineWidth = "3";
-  //   ctx.moveTo(scale*(i+1), 0);
-  //   ctx.lineTo(height, scale*(i+1));
-  //   ctx.stroke();
-  // }
+  //Draws rows
+  for(i = 0; i < rows; i++){
+      ctx.beginPath();
+      ctx.lineWidth = ".5";
+      ctx.strokeStyle = "black";
+      ctx.moveTo(0, scale*(i+1));
+      ctx.lineTo(width, scale*(i+1));
+      ctx.stroke();
+  }
+  //Draws columns
+  for(i = 0; i < columns; i++){
+      ctx.beginPath();
+      ctx.lineWidth = ".5";
+      ctx.strokeStyle = "#000";
+      ctx.moveTo(scale*(i+1)+0.5, 0);
+      ctx.lineTo(scale*(i+1)+0.5, height);
+      ctx.stroke();
+  }
 
-  // ctx.beginPath();
-  //  ctx.lineWidth = ".5";
-  //  ctx.moveTo(50, 0);
-  //  ctx.lineTo(height, 50);
-  //  ctx.stroke();
+
 
 }
 
 function draw() {
   snake.drawSnake();
   drawGrid();
-  console.log(rows);
+  console.log(columns);
 }
 
 draw();
